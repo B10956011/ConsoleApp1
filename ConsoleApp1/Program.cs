2f2cc8ba84ca9be750964b52ab9169c99a58fae2
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:06:57 +0800

    練習

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string test="test"
+            Console.WriteLine("hello world");
+            string test = "test";
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int total = (int)(meal_cost+tip_percent+tax_percent);
+            Console.WriteLine("用餐費用：" + meal_cost);
+            Console.WriteLine("服務費：" + tip_percent);
+            Console.WriteLine("稅率：" + tax_percent);
+
+            Console.WriteLine("總共：" + total);
+
+
         }
     }
 }
