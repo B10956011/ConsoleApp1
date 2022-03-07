3d69d812df309d518d03f7e973e8a79eae7feea1
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:08:27 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int total = (int)(meal_cost + tip_percent + tax_percent);
+            Console.WriteLine("用餐費用：" + meal_cost);
+            Console.WriteLine("服務費：" + tip_percent);
+            Console.WriteLine("稅率：" + tax_percent);
+            Console.WriteLine("總共：" + total);
+
         }
     }
 }
